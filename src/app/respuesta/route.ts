07282d7fef075c5b469eddc6f3cc897461d7559c
Jonatan Ariste <jonatan.ariste@gmail.com>
2024-06-02 23:19:19 -0300

    Litel ajustment for win X8

--- src/app/respuesta/route.ts
+++ src/app/respuesta/route.ts
@@ -1,30 +1,40 @@
 import type { NextRequest } from "next/server";
 import { MercadoPagoConfig, Payment } from "mercadopago";
-export const dynamic = "force-dynamic";
+import axios from "axios";
+
 const mercadopago = new MercadoPagoConfig({
   accessToken:
     "APP_USR-8928404133394808-030200-82b0e5e66c5ef336eff7814e687319e3-225552793",
 });
 
 async function postData(url = "", data = {}) {
+  let loqueresulta = await axios
+    .post(url, data)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return loqueresulta;
   // Default options are marked with *
-  console.log("postData-a:", url);
-  const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
+  // console.log("postData-a:", url);
+  // const response = await fetch(url, {
+  //   method: "POST", // *GET, POST, PUT, DELETE, etc.
+  //   mode: "cors", // no-cors, *cors, same-origin
+  //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+  //   credentials: "same-origin", // include, *same-origin, omit
+  //   headers: {
+  //     "Content-Type": "application/json",
 
-      // "Content-Type": "application/x-www-form-urlencoded",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-  return response;
+  //     // "Content-Type": "application/x-www-form-urlencoded",
+  //     // 'Content-Type': 'application/x-www-form-urlencoded',
+  //   },
+  //   redirect: "follow", // manual, *follow, error
+  //   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  //   body: JSON.stringify(data), // body data type must match "Content-Type" header
+  // });
+  // return response;
 }
 export async function POST(request: NextRequest) {
   const body = await request
@@ -59,7 +69,7 @@ export async function POST(request: NextRequest) {
   console.log("payment.status:", payment.status);
   if (payment && payment.status == "approved") {
     postData(process.env.API_URL, datita).then((data) => {
-      //console.log("Lo que respondió Firebase: ", data); // JSON data parsed by `data.json()` call
+      console.log("Lo que respondió Firebase: ", data); // JSON data parsed by `data.json()` call
     });
   }
 
